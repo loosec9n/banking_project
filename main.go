e0e681dacf5a42d16f2395abad8aa958d00051aa
Justin John <activejobckr@gmail.com>
2022-10-05 14:21:23 +0530

    swagger for grpc gateway implementation

--- main.go
+++ main.go
@@ -75,6 +75,9 @@ func runGatewayServer(config utils.Config, store *db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
+	fs := http.FileServer(http.Dir("./doc/swagger"))
+	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start the listener in GRPC server : ", err)
